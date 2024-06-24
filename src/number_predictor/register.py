a444bca2eed8e7a65146625ae8d070bb047fa413
Dean van Aswegen <53308143+Deanis@users.noreply.github.com>
2024-06-24 06:57:13 +0200

    chore: Update Azure ML settings in register.py

--- src/number_predictor/register.py
+++ src/number_predictor/register.py
@@ -27,12 +27,12 @@ def register_model_if_accuracy_above_threshold(
         print("Model accuracy is above threshold, registering model.")
 
         # Define your Azure ML settings
-        subscription_id = "0a94de80-6d3b-49f2-b3e9-ec5818862801"
+        subscription_id = ""
         resource_group = "buas-y2"
         workspace_name = "Staff-Test"
-        tenant_id = "0a33589b-0036-4fe8-a829-3ed0926af886"
-        client_id = "a2230f31-0fda-428d-8c5c-ec79e91a49f5"
-        client_secret = "Y-q8Q~H63btsUkR7dnmHrUGw2W0gMWjs0MxLKa1C"
+        tenant_id = ""
+        client_id = ""
+        client_secret = ""
 
         credential = ClientSecretCredential(tenant_id, client_id, client_secret)
 
